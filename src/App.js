7e521dcae418e33b946ab155c424dc06f63e10ca
Chandrakishor Tiwari <chandrkishor@mindrops.com>
2022-11-07 08:59:59 +0530

    urlShortener Frontend done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AddUrlComponent from "./components/AddUrlComponent";
+import ViewUrlComponent from "./components/ViewUrlComponent";
+import { useState } from "react";
 
 function App() {
+  const [refreshList, setRefreshList] = useState(true);
+
+  function refreshListFun() {
+    setRefreshList((prev) => !prev);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container mt-5">
+      <AddUrlComponent refreshListFun={refreshListFun} />
+      <ViewUrlComponent refreshList={refreshList} />
     </div>
   );
 }
